967e41df4908dc6b563063a0794441845e20a3f6
Orestis Tsirakis <orestistsira@gmail.com>
2023-02-02 00:24:00 +0200

    serial d4

--- main.cpp
+++ main.cpp
@@ -41,8 +41,10 @@
 // }
 
 //for csr (csc only for symmetric matrices)
-void compute_square(mwIndex *row, mwIndex *col, double *val, mwSize n, mwSize m, mwIndex *rowRes, mwIndex *colRes, double *valRes){
+void compute_square(mwIndex *row, mwIndex *col, mwSize n, mwSize m, mwIndex *rowRes, mwIndex *colRes, double *valRes){
     int ip = 0;
+
+    double* d = (double*) calloc(n, sizeof(double));
 
     int *xb = (int*) malloc(n * sizeof(int));
     for(int i = 0; i < n; i++){
@@ -50,34 +52,63 @@ void compute_square(mwIndex *row, mwIndex *col, double *val, mwSize n, mwSize m,
     }
     int *x = (int*) malloc(n * sizeof(int));
 
+    //for each column (i)
     for(int i = 0; i < n; i++){
+        printf("i = %d:\n", i);
         colRes[i] = ip;
 
+        //for each row (j) that column (i) has an element
         for(int jp = col[i]; jp < col[i+1];jp++){
             int j = row[jp];
+            printf("j = %d\n", j);
 
+            //for each row (k) that column (j) has an element
             for(int kp = col[j]; kp < col[j+1]; kp++){
                 int k = row[kp];
+                printf("k = %d\n", k);
+
+                //if(k == 1 && i == 1) continue;
+                bool compute = false;
+                for(int fp = col[i]; fp < col[i+1]; fp++){
+                    int f = row[fp];
+                    if(f == k){
+                        compute = true;
+                        break;
+                    } 
+                }
+
+                if(!compute) continue;
 
                 if(xb[k] != i){
+                    printf("Loaded element (%d, %d)\n", k, i);
                     rowRes[ip] = k;
                     ip += 1;
                     xb[k] = i;
-                    x[k] = val[jp] * val[kp];
+                    //1 = valxval
+                    x[k] = 1;
                 }
                 else{
-                    x[k] = x[k] + (val[jp] * val[kp]);
+                    //1 = valxval
+                    x[k] = x[k] + 1;
                 }
             }
         }
 
-        //if()
         for(int vp = colRes[i]; vp < ip; vp++){
             int v = rowRes[vp];
 
             valRes[vp] = x[v];
+            d[i] += x[v];
         }
+
+        printf("\n");
     }
+    printf("\n");
+
+    for(int i = 0; i < n; i++){
+        printf("%f ", d[i]);
+    }
+    printf("\n");
 
     colRes[n] = ip;
 }
@@ -96,7 +127,7 @@ void printArrayD(double* array, mwSize n){
     printf("\n");
 }
 
-const int m = 3;
+const int m = 4;
 const int n = 3;
 
 int main(int argc, char** argv){
@@ -111,7 +142,7 @@ int main(int argc, char** argv){
 
     printf("here\n");
 
-    compute_square(row, col, val, n, m, rowRes, colRes, valRes);
+    compute_square(row, col, n, m, rowRes, colRes, valRes);
 
     int mRes = colRes[n];
 
